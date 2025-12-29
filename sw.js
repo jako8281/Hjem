75b4b6d0abe73212ae60ab8e766bcd71c75bc552
Jakob Beith Severinsen <jakbeisev@gmail.com>
2025-12-29 12:30:28 +0100

    Refactor service worker to remove caching
    Removed caching logic and added console log on install.

--- sw.js
+++ sw.js
@@ -1,23 +1,7 @@
-const CACHE_NAME = 'rengoering-v1';
-const ASSETS = [
-  '/Hjem/',
-  '/Hjem/index.html',
-  '/Hjem/styles.css',
-  '/Hjem/app.js',
-  '/Hjem/manifest.json',
-  '/Hjem/icons/icon-192.png',
-  '/Hjem/icons/icon-512.png'
-];
-self.addEventListener('install', (e)=>{
-  e.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)));
+self.addEventListener('install', event => {
+  console.log('Service worker installed');
 });
-self.addEventListener('activate', (e)=>{
-  e.waitUntil(
-    caches.keys().then(keys=>Promise.all(keys.map(k=>{ if(k!==CACHE_NAME) return caches.delete(k); })))
-  );
-});
-self.addEventListener('fetch', (e)=>{
-  e.respondWith(
-    caches.match(e.request).then(resp=> resp || fetch(e.request))
-  );
+
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request));
 });
