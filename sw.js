f9b4985e69bb140b44c220b8369f3d3bfede84d6
Jakob Beith Severinsen <jakbeisev@gmail.com>
2025-12-29 11:01:45 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'rengoering-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './styles.css',
+  './app.js',
+  './manifest.webmanifest',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+self.addEventListener('install', (e)=>{
+  e.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)));
+});
+self.addEventListener('activate', (e)=>{
+  e.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.map(k=>{ if(k!==CACHE_NAME) return caches.delete(k); })))
+  );
+});
+self.addEventListener('fetch', (e)=>{
+  e.respondWith(
+    caches.match(e.request).then(resp=> resp || fetch(e.request))
+  );
+});
